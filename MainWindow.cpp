56d53e31ade39cc41566910baafac8308268a035
kahuz <67911543+kahuz@users.noreply.github.com>
2020-07-26 09:35:33 +0900

    modularize image viwer and ui update
    이미지 뷰어에 대해 클래스 분할 작업 및 스레드 작업 완료
    이미지 재생 도구 추가 및 라벨링 도구 추가
    도구를 이용한 라벨링 작업 구현 필요

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,74 +1,89 @@
-#pragma comment(linker , "/entry:WinMainCRTStartup /subsystem:console")
+#pragma comment(linker , "/entry:WinMainCRTStartup /subsystem:console")	//for debugging
 
 #include "MainWindow.h"
 #include <QDesktopWidget>
-//opencv lib
-#include <opencv2/core.hpp>		// basic OpenCV structures ( Mat , Scalar, etc... )
-#include <opencv2/videoio.hpp>	// for VideoCapture, VideoWriter
-#include <opencv2/highgui.hpp>	// OpenCV Window IO
-#include <opencv2/opencv.hpp>
-//cpp lib
-#include <iostream>
-#include <string>
 
-using namespace cv;
-
-//출력할 영상을 읽어올 경로 변수 선언 및 초기화
-std::string video_path("C:\\Users\\kahu\\Videos\\driving\\bus.mp4");
-//출력한 프레임 수를 저장할 변수 선언 및 초기화
-int frame_cnt = 0;
-
-//param event : mouse event
-//param x, y : mouse pos
-//param flags : mouse flags
-//param user_data : 추가적으로 전달받을 데이터
-
-void MainWindow::view_video(std::string path)
+void MainWindow::open_video(std::string path)
 {
-	//영상 경로를 전역변수가 아닌 입력에 따라 변경할 경우 아래 주석을 따르면 된다
-//cout << "input file path\n";
-//cin >> video_path;
-//cout << "file path is " << video_path << endl;
-//video_path의 영상 파일 정보를 가져온다. CAP_FFMPEG는 영상 파일을 읽어오거나 FFPMEG 라이브러리를 이용해 스트리밍을 제공
-//단순 파일을 읽어올 경우 CAP_FFMPEG 플래그를 생략해도 상관 없다
-	VideoCapture bus_video(path);
+	if (main_viewer.video.isOpened())
+		main_viewer.video.release();
 
-	//bus_video에 연결된 영상 파일로부터 프레임 이미지를 받아올 Mat 변수 선언
-	Mat bus_image;
+	main_viewer.video.open(path);
 
-	//video_path로 부터 파일 정보를 가져오지 못할 경우
-	if (!bus_video.isOpened())
+	if (!main_viewer.video.isOpened())
 	{
-		std::cout << "Could not open video" << video_path << std::endl;
-	}
-	bool is_play = true;
-	//while을 돌며 bus_video로부터 프레임 이미지를 읽어옴
-	while (is_play)
-	{
-		//bus_video로부터 하나의 프레임을 읽어 bus_image에 저장
-		bus_video >> bus_image;
-		//bus_image가 전달받은 이미지가 없다면
-		if (bus_image.empty())
+		QMessageBox MsgBox;
+		MsgBox.setText("Could not open video/image");
+		MsgBox.setStandardButtons(QMessageBox::Ok);
+		MsgBox.setDefaultButton(QMessageBox::Ok);
+
+		if (MsgBox.exec() == QMessageBox::Ok)
 		{
-			std::cout << "video end" << std::endl;
-			break;
+			MsgBox.close();
+		}
+	}
+
+	Mat viewer_img;
+
+	main_viewer.video >> viewer_img;
+
+	ui.mainLabel->setPixmap(QPixmap::fromImage(QImage(viewer_img.data, viewer_img.cols, viewer_img.rows, viewer_img.step, QImage::Format_BGR888)));
+
+	main_viewer.set_runable(true);
+	main_viewer.set_running(PAUSE);
+	main_viewer.set_viwer(ui.mainLabel);
+
+	this->repaint();
+}
+//note this->repaint()
+void MainWindow::play_video(QAction* button, bool play)
+{
+	std::cout << __FUNCTION__ ;
+	if (play == PLAY)
+	{
+		std::cout << "  Play is True , is_running : " << main_viewer.is_running() << " is_runable : " << main_viewer.is_runable() << std::endl;
+		//재생 중일 때 재생 버튼을 클릭되지 않은 상태로 돌린다
+		if (main_viewer.is_running())
+		{
+			std::cout << " already play " << std::endl;
+			button->setChecked(true);
+			return;
 		}
 
-		//opencv로 받아온 이미지는 BGR 형태로 저장되므로 마지막 플래그를 BFR888로 전달하여 이미지 저장
-		ui.mainLabel->setPixmap(QPixmap::fromImage(QImage(bus_image.data, bus_image.cols, bus_image.rows, bus_image.step, QImage::Format_BGR888)));
-		//비디오 프레임 카운트 변수, 생략해도 상관없다
-		frame_cnt++;
-		//비디오 프레임 출력
-		std::cout << "Frame: " << frame_cnt << "\n";
-		//비디오를 읽어올 지연 속도를 waitKey로 지정
-		//33으로 지정한 것은 약 30fps 속도로 출력하기 위함
-		char c = (char)waitKey(33);
-		//이미지가 변경됬으므로 mainwindow의 컴포넌트를 refrash하기 위해 show 함수 호출
-		this->show();
-		// esc 키 입력이 들어오면 while 종료구문
-		// QT event 형식으로 변경 예정
-		if (c == 27)
-			is_play = false;
+		if (main_viewer.is_runable())
+		{
+			std::cout << " now playing video " << std::endl;
+			main_viewer.set_running(PLAY);
+
+			std::thread video_thread(&Viewer::show, &main_viewer);
+
+			video_thread.detach();
+		}
+	}
+	else if (play == PAUSE)
+	{
+		if (main_viewer.is_running())
+		{
+			//일시중지
+			main_viewer.set_running(PAUSE);
+		}
+		//재생 중지 혹은 비디오가 출력중이지 않을때 멈춤 버튼을 클릭되지 않은 상태로 돌린다
+		button->setChecked(true);
+		return;
+	}
+
+	else if (!main_viewer.video.isOpened())
+	{
+		QMessageBox MsgBox;
+		MsgBox.setText("Please, open the video/image file");
+		MsgBox.setStandardButtons(QMessageBox::Ok);
+		MsgBox.setDefaultButton(QMessageBox::Ok);
+
+		if (MsgBox.exec() == QMessageBox::Ok)
+		{
+			MsgBox.close();
+			button->setChecked(false);
+		}
 	}
 }
 
@@ -103,13 +118,22 @@ MainWindow::MainWindow(QWidget* parent)
 	QDesktopWidget desktop;
 
 	ui.setupUi(this);
-
 	label_dialog.move(desktop.screenGeometry().width() / 2 + this->width() / 2, desktop.screenGeometry().height() / 2 - this->height() / 2);
 
 	connect(ui.actionOpen_Label_View, SIGNAL(triggered()), this, SLOT(on_triggered_menu_openLabelView()));
 	connect(ui.actionOpen_Video_Image, SIGNAL(triggered()), this, SLOT(on_triggered_menu_openVideoImage()));
 	connect(ui.actionOpen_Images_Directory, SIGNAL(triggered()), this, SLOT(on_triggered_menu_openVideoImage()));
-	
+	connect(ui.actionVideoPlay, SIGNAL(triggered()), this, SLOT(on_triggered_menu_videoPlay()));
+	connect(ui.actionVideoPause, SIGNAL(triggered()), this, SLOT(on_triggered_menu_videoPause()));
+}
+
+MainWindow::~MainWindow()
+{
+	if (main_viewer.is_runable())
+		main_viewer.set_runable(false);
+
+	if (main_viewer.is_running())
+		main_viewer.set_running(false);
 }
 
 //Trigger Functions
@@ -121,7 +145,7 @@ void MainWindow::on_triggered_menu_openVideoImage()
 		"Select source file",
 		".",
 		"All files (*.*)");
-	view_video(video_img_path.toStdString());
+	open_video(video_img_path.toStdString());
 
 	std::cout << video_img_path.toStdString() << std::endl;
 }
@@ -142,4 +166,63 @@ void MainWindow::on_triggered_menu_openLabelView()
 {
 	std::cout << "on_triggered_menu_openLabelView" << std::endl;
 	label_dialog.show();
+}
+
+
+void MainWindow::on_triggered_menu_videoPlay()
+{
+	play_video(ui.actionVideoPlay, PLAY);
+}
+
+void MainWindow::on_triggered_menu_videoPause()
+{
+	play_video(ui.actionVideoPause, PAUSE);
+}
+
+void Viewer::show()
+{
+	while (b_runable)
+	{
+		while (b_running == PAUSE) { Sleep(100); };
+
+		Mat viewer_img;
+		this->video >> viewer_img;
+		std::cout << "asdf " << std::endl;
+		this->viwer->setPixmap(QPixmap::fromImage(QImage(viewer_img.data, viewer_img.cols, viewer_img.rows, viewer_img.step, QImage::Format_BGR888)));
+		//30fps
+		Sleep(33);
+	}
+
+	this->set_runable(true);
+}
+
+bool Viewer::is_running()
+{
+	return this->b_running;
+}
+
+bool Viewer::is_runable()
+{
+	return this->b_runable;
+}
+
+void Viewer::set_running(bool is_running)
+{
+	if (is_running)
+		this->b_running = true;
+	else
+		this->b_running = false;
+}
+
+void Viewer::set_runable(bool is_runable)
+{
+	if (is_runable)
+		this->b_runable = true;
+	else
+		this->b_runable = false;
+}
+
+void Viewer::set_viwer(QLabel* parent_viwer)
+{
+	this->viwer = parent_viwer;
 }
--- MainWindow.h
+++ MainWindow.h
@@ -1,30 +1,73 @@
 #pragma once
+//QT lib
 #include <QtWidgets/QMainWindow>
-#include <QPixmap>
-
 #include <QMessageBox>
 #include <QFileDialog>
+#include <QPixmap>
+//OpenCV lib
+#include <opencv2/core.hpp>		// basic OpenCV structures ( Mat , Scalar, etc... )
+#include <opencv2/videoio.hpp>	// for VideoCapture, VideoWriter
+#include <opencv2/highgui.hpp>	// OpenCV Window IO
+#include <opencv2/opencv.hpp>
+//cpp lib
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <thread>
+//user lib
 #include "ui_MainWindow.h"
 #include "LabelDialog.h"
 
+#define PLAY    true
+#define PAUSE   false
 
+using namespace cv;
+
+class Viewer
+{
+public:
+
+    void show();
+    bool is_running();
+    bool is_runable();
+
+    void set_running(bool is_running);
+    void set_runable(bool is_runable);
+    void set_viwer(QLabel* parent_viwer);
+    VideoCapture video;
+
+private:
+    QLabel *viwer;
+
+    bool b_running = false;
+    bool b_runable = false;
+};
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 public:
     MainWindow(QWidget* parent = Q_NULLPTR);
-    void MainWindow::view_video(std::string path);
+    ~MainWindow();
+    void open_video(std::string path);
+    void play_video(QAction* button, bool play);
 
 private slots:
     void on_triggered_menu_openLabelView();
     void on_triggered_menu_openVideoImage();
     void on_triggered_menu_openImagesDirectory();
 
+    void on_triggered_menu_videoPlay();
+    void on_triggered_menu_videoPause();
+
 private:
-    Ui::MainWindow ui;
-    LabelDialog label_dialog;
     void MainWindow::video_mouse_callback(int event, int x, int y, int flags, void* user_data);
 
     const QString main_title = "MainWindow Title";
+    LabelDialog label_dialog;
+    bool main_video_play = false;
+
+    Viewer main_viewer;
+
+    Ui::MainWindow ui;
 };
 
