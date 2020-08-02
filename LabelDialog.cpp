8f6ba93876cc2d8375c11a55adbae73baceb9c2d
kahuz <67911543+kahuz@users.noreply.github.com>
2020-08-02 19:46:23 +0900

    Update label dialog
    라벨링 된 이미지들에 대해 미리보기를 제공하는 다이어로그
    MainWindow의 Video/Img Viewer로부터 취득한 라벨 이미지를 보여준다

--- LabelDialog.cpp
+++ LabelDialog.cpp
@@ -9,3 +9,19 @@ LabelDialog::LabelDialog(QWidget *parent)
 LabelDialog::~LabelDialog()
 {
 }
+
+bool LabelDialog::get_is_open()
+{
+	return m_is_open;
+}
+
+void LabelDialog::set_is_opne(bool is_open)
+{
+	m_is_open = is_open;
+}
+
+void LabelDialog::draw_prev_img(QPixmap prev_img)
+{
+	ui.prev_label->setPixmap(prev_img);
+	ui.prev_label->update();
+}
--- LabelDialog.h
+++ LabelDialog.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#ifndef __LABEL_DIALOG__
+#define __LABEL_DIALOG__
+
+//QT lib
 #include <QWidget>
+#include <QPixmap>
 #include "ui_LabelDialog.h"
+
 
 class LabelDialog : public QWidget
 {
@@ -11,6 +17,12 @@ public:
 	LabelDialog(QWidget *parent = Q_NULLPTR);
 	~LabelDialog();
 
+	bool get_is_open();
+	void set_is_opne(bool is_open);
+	void draw_prev_img(QPixmap prev_img);
 private:
+	bool m_is_open = false;
 	Ui::LabelDialog ui;
 };
+
+#endif
